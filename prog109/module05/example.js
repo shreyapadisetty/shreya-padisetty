033b9cca1ce1b96cc1b0c32d46c9fa7b7e289dd3
Shreya Padisetty <79680081+shreypy@users.noreply.github.com>
2021-05-07 17:11:11 -0700

    Update example.js

--- prog109/module05/example.js
+++ prog109/module05/example.js
@@ -4,19 +4,21 @@ var operator = 'multiplication'; // Type of calculation
 var i = 1;                 // Set counter to 1
 var msg = '<h2>Multiplication Table</h2>';    // Message
 
+/*
 if (operator === 'addition') {
   // Do addition
   while (i < 11) {
     msg += i + ' + ' + table + ' = ' + (i + table) + '<br />';
     i++;
   }
-} else {
+}
+*/
+
   // Do multiplication
   while (i < 11) {
     msg += i + ' x ' + table + ' = ' + (i * table) + '<br />';
     i++;
   }
-}
 
 // Write the message into the page
 var el = document.getElementById('blackboard');
