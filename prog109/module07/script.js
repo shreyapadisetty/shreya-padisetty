98a442a7257aaa0d5f74c9c804f62fe40121cb9b
Shreya Padisetty <79680081+shreypy@users.noreply.github.com>
2021-05-22 10:36:27 -0700

    Update script.js

--- prog109/module07/script.js
+++ prog109/module07/script.js
@@ -7,7 +7,39 @@
    validFirstname = true;
  }
  document.getElementById("errorMessages").innerHTML = errorMessages;
- return (validFirstname);
+
+ var validLastname=false;
+ var validLastname = document.getElementById("lastname").value;
+ if (lastname==="null" || lastname==="" || lastname.length > 20) {
+   errorMessages += "<p>The lastname is required and cannot be greater than 20 characters</p>";
+ } else {
+   validLastname = true;
+ }
+ document.getElementById("errorMessages").innerHTML = errorMessages;
+ return (validFirstname && validLastname);
+
+var userEmail = document.getElementById("email").value;
+var atpos = userEmail.indexOf("@");
+var dotpos = userEmail.lastIndexOf(".");
+if (atpos< 1 || dotpos<atpos+2 || dotpos+2>=userEmail.length) {
+    errorMessages = "<p>Invalid email</p>";
+} else {
+    return true; 
+}
+
+var phone = document.getElementById("phone").value;
+if (isNaN(phone) || phone.lenght >15 || phone===null || phone==="")
+	errorMessages = "<p>Invalid phone number </p>";
+else
+  return true; 
+/*
+var numbers=/^[0-9]+$/;
+if (phone===null||phone ==="" ||phone.length>15|| !phone.match(numbers)) {
+	errorMessages = "<p>Invalid phone number </p>";
+} else {
+   return true;
+}
+*/
 
  frmvalidator.addValidation("FirstName","req","Please enter your First Name");
  frmvalidator.addValidation("FirstName","maxlen=20",
